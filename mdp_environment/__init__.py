5b0b88683fdb1f6b0906f6e018824c30fa3e8fff
Rishav Chourasia <rishav.chourasia@gmail.com>
2019-01-13 21:27:13 +0100

    feat: adding mdp-v4 variats with different action set sizes

--- mdp_environment/__init__.py
+++ mdp_environment/__init__.py
@@ -19,3 +19,28 @@ register(
 register(
     id='mdp-v4',
     entry_point='mdp_environment.envs:MdpEnvLinCorridor')
+
+register(
+    id='mdp:2-v4',
+    entry_point='mdp_environment.envs:MdpEnvLinCorridor',
+    kwargs={"size": 25, "dim": 2})
+
+register(
+        id='mdp:5-v4',
+    entry_point='mdp_environment.envs:MdpEnvLinCorridor',
+    kwargs={"size": 25, "dim": 5})
+
+register(
+        id='mdp:10-v4',
+    entry_point='mdp_environment.envs:MdpEnvLinCorridor',
+    kwargs={"size": 25, "dim": 10})
+
+register(
+        id='mdp:25-v4',
+    entry_point='mdp_environment.envs:MdpEnvLinCorridor',
+    kwargs={"size": 25, "dim": 25})
+
+register(
+        id='mdp:50-v4',
+    entry_point='mdp_environment.envs:MdpEnvLinCorridor',
+    kwargs={"size": 25, "dim": 50})
